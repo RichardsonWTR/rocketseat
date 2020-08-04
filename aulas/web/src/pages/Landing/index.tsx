f348e46463ee4e60663de298c56997bb4c614987
richardsonwtr <richardsonwtr@gmail.com>
2020-08-03 23:02:19 -0300

    Adding routes

--- aulas/web/src/pages/Landing/index.tsx
+++ aulas/web/src/pages/Landing/index.tsx
@@ -6,6 +6,7 @@ import studyIcon from '../../assets/images/icons/study.svg'
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg'
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg'
 import './styles.css'
+import {Link} from 'react-router-dom'
 
 function Landing() {
     return (
@@ -19,14 +20,14 @@ function Landing() {
                 <img className="hero-image" src={landingImg} alt="Plataforma de estudos"/>
 
                 <div className="buttons-container">
-                    <a href="" className="study">
+                    <Link to="/study" className="study">
                         <img src={studyIcon} alt="Estudar"/>
                         Estudar
-                    </a>
-                    <a href="" className="give-classes">
+                    </Link>
+                    <Link to="/give-classes" className="give-classes">
                         <img src={giveClassesIcon} alt="Dar aulas"/>
                         Dar aulas
-                    </a>
+                    </Link>
                 </div>
 
                 <span className="total-connections">
