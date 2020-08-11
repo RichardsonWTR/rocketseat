696deb6bae27e17fba83b215a05894790e6d0c28
richardsonwtr <richardsonwtr@gmail.com>
2020-08-10 22:38:43 -0300

    Implementing shared page header

--- src/routes/StudyTabs.tsx
+++ src/routes/StudyTabs.tsx
@@ -39,8 +39,8 @@ function StudyTabs() {
             <Screen 
              options={{
                  tabBarLabel: 'Proffys',
-                 tabBarIcon: ({ color, size }) =>{
-                     return <Ionicons name="ios-easel" size={size} color={color} /> 
+                 tabBarIcon: ({ color, size, focused }) =>{
+                     return <Ionicons name="ios-easel" size={size} color={focused ? '#8257e5' : color} /> 
                  }
              }}
              name="TeacherList"
@@ -48,8 +48,8 @@ function StudyTabs() {
             <Screen 
              options={{
                 tabBarLabel: 'Favoritos',
-                tabBarIcon: ({ color, size }) =>{
-                    return <Ionicons name="ios-heart" size={size} color={color} /> 
+                tabBarIcon: ({ color, size, focused }) =>{
+                    return <Ionicons name="ios-heart" size={size} color={focused ? '#8257e5' : color} /> 
                 }
             }}
              name="Favorites"
